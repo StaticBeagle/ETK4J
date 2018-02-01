49d86b1e2f8199dcbc0064b176b4cc72b7259488
StaticBeagle <m.lopez27@gmail.com>
2018-02-01 09:30:33 -0600

    Moving examples into src

---
+++ src/examples/FourmulasExample.java
@@ -0,0 +1,29 @@
+package examples;
+
+import static com.wildbitsfoundry.etk4j.math.Formulas.quadraticFormula;
+
+import com.wildbitsfoundry.etk4j.math.complex.Complex;
+
+public class FourmulasExample {
+	
+	public static void main(String[] args) {
+		double a = 1.0;
+		double b = 3.0;
+		double c = -4;
+		
+		// a * x^2 + b * x + c 
+		// roots should be [-4, 1]
+		System.out.println("Real distinct roots");
+		for(Complex root : quadraticFormula(a, b, c)) {
+			System.out.println(root);
+		}
+		
+		// imaginary roots 
+		// roots should be [-0.5000 + 0.8660i, -0.5000 - 0.8660i]
+		System.out.printf("%nComplex roots %n");
+		for(Complex root : quadraticFormula(1, 1, 1)) {
+			System.out.println(root);
+		}
+		
+	}
+}
