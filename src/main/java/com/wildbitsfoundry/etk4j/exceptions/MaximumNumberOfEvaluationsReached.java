d90df4bb606eefb166b193550ef6e6df46bd5ed3
StaticBeagle <m.lopez27@gmail.com>
2021-01-08 11:53:37 -0600

    Added max number of evaluations reached exception

---
+++ src/main/java/com/wildbitsfoundry/etk4j/exceptions/MaximumNumberOfEvaluationsReached.java
@@ -0,0 +1,8 @@
+package com.wildbitsfoundry.etk4j.exceptions;
+
+public class MaximumNumberOfEvaluationsReached extends RuntimeException {
+
+    public MaximumNumberOfEvaluationsReached(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/wildbitsfoundry/etk4j/exceptions/MaximunNumberOfEvaluationsReached.java
+++
@@ -1,8 +0,0 @@
-package com.wildbitsfoundry.etk4j.exceptions;
-
-public class MaximunNumberOfEvaluationsReached extends RuntimeException {
-
-    public MaximunNumberOfEvaluationsReached(String message) {
-        super(message);
-    }
-}
