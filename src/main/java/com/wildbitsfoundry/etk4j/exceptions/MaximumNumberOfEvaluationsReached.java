fbdfa39c62ec93d7b222312ffa68eab66b7ce09c
Marcos <m.lopez27@gmail.com>
2021-01-16 14:55:25 -0600

    Supressed the serial warning

--- src/main/java/com/wildbitsfoundry/etk4j/exceptions/MaximumNumberOfEvaluationsReached.java
+++ src/main/java/com/wildbitsfoundry/etk4j/exceptions/MaximumNumberOfEvaluationsReached.java
@@ -1,5 +1,6 @@
 package com.wildbitsfoundry.etk4j.exceptions;
 
+@SuppressWarnings("serial")
 public class MaximumNumberOfEvaluationsReached extends RuntimeException {
 
     public MaximumNumberOfEvaluationsReached(String message) {
