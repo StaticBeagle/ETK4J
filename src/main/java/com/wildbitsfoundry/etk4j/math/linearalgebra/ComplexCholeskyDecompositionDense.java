08875b1f869b5c77766e1e68290d51d38a848d73
StaticBeagle <m.lopez27@gmail.com>
2024-12-22 07:47:35 -0600

    Added unit test Complex Cholesky Decomposition

--- src/main/java/com/wildbitsfoundry/etk4j/math/linearalgebra/ComplexCholeskyDecompositionDense.java
+++ src/main/java/com/wildbitsfoundry/etk4j/math/linearalgebra/ComplexCholeskyDecompositionDense.java
@@ -78,37 +78,4 @@ public class ComplexCholeskyDecompositionDense extends ComplexCholeskyDecomposit
         // Solve R * X = Y;
         return backSubstitutionSolve(R, Y);
     }
-
-    public static void main(String[] args) {
-        ComplexMatrixDense Arg = ComplexMatrixDense.from2DArray(new Complex[][]{
-                {new Complex(0.9767407521087846, 0.4711470863081668), new Complex(0.2794308404798361, 0.4587985898568102), new Complex(0.1685348957730604, 0.2220415588608931), new Complex(0.8646965674039528, 0.5496275298999502)},
-                {new Complex(0.7198733467511716, 0.9814006625666408), new Complex(0.01506799866339292, 0.666774973911552), new Complex(0.750545968546426, 0.4842583141948102), new Complex(0.8173900466200722, 0.9463910637201288)},
-                {new Complex(0.3444764277139777, 0.7365028309538496), new Complex(0.8429046576872399, 0.1345623113997281), new Complex(0.6191817846917982, 0.174500592061623), new Complex(0.1931980127831494, 0.3833402493069025)},
-                {new Complex(0.2529620785541479, 0.8308769763271352), new Complex(0.7385598627121508, 0.850513285400206), new Complex(0.3669833799842998, 0.3169573549259639), new Complex(0.4630012679866645, 0.4444797942481419)}
-        });
-
-        // Making it symmetric
-        System.out.println("Original Matrix");
-        Arg.multiplyEquals(Arg.conjugateTranspose());
-        System.out.println(Arg);
-
-        ComplexCholeskyDecompositionDense choleskyDecompositionDense = new ComplexCholeskyDecompositionDense(Arg);
-        ComplexMatrixDense R = choleskyDecompositionDense.getR();
-        System.out.println("R");
-        System.out.println(R);
-        System.out.println("RH * R");
-        System.out.println(R.conjugateTranspose().multiply(R));
-        System.out.println("Is symmetric");
-        System.out.println(choleskyDecompositionDense.isSPD());
-
-        ComplexMatrixDense b = ComplexMatrixDense.from2DArray(new Complex[][] {
-                {new Complex(90, 90)},
-                {new Complex(80, 80)},
-                {new Complex(70, 70)},
-                {new Complex(60, 60)}
-        });
-
-        ComplexMatrixDense X = choleskyDecompositionDense.solve(b);
-        System.out.println(X);
-    }
 }
