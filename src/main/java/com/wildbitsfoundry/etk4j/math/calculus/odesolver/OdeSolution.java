ed378af1fc6e0541772029b894254a148183e6ad
StaticBeagle <m.lopez27@gmail.com>
2025-03-08 08:36:50 -0600

    Updated 2D array to MatrixSparse

--- src/main/java/com/wildbitsfoundry/etk4j/math/calculus/odesolver/OdeSolution.java
+++ src/main/java/com/wildbitsfoundry/etk4j/math/calculus/odesolver/OdeSolution.java
@@ -1,7 +1,6 @@
-package com.wildbitsfoundry.etk4j.math.calculus.odesolvers;
+package com.wildbitsfoundry.etk4j.math.calculus.odesolver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class OdeSolution {
--- src/main/java/com/wildbitsfoundry/etk4j/statistic/regression/PolynomialRegression.java
+++ src/main/java/com/wildbitsfoundry/etk4j/statistic/regression/PolynomialRegression.java
@@ -18,17 +18,4 @@ public class PolynomialRegression extends UnivariateRegression {
         MatrixDense X = MatrixDense.Factory.vandermonde(x, rows, n + 1);
         this.doRegression(X, x, y);
     }
-
-    public static void main(String[] args) {
-        double[] x ={1, 2, 3, 4};
-        double[] y = {1, 3.5, 8.4, 17.2};
-        PolynomialRegression polynomialRegression = new PolynomialRegression(x, y, 2);
-        System.out.println(polynomialRegression.R2());
-        System.out.println(polynomialRegression.R());
-        System.out.println(Arrays.toString(polynomialRegression.beta()));
-        System.out.println(Arrays.toString(polynomialRegression.residuals()));
-        System.out.println(polynomialRegression.SSE());
-        System.out.println(polynomialRegression.SSR());
-        System.out.println(polynomialRegression.SST());
-    }
 }
